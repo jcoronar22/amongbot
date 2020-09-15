b5fc85d880ec4576ccde05f316b3fa7e24e052e6
Juan Pablo Corona <pcorona@occ.com.mx>
2020-09-15 13:31:49 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Among us bot
+
+This is a sample bot for remind to play among us
+
+##### Requirements
+- NodeJS properly installed
+
+##### Getting started
+On terminal run:
+`npm install`
+
+Configure the proper environment variables.
+First of all, copy the _.env.example_ file provided in the repo
+`cp .env.example .env`
+And fill the required configuration variables
+
+Note: _.env_ files doesn't must never be uploaded to a repo for security 
+
+After package installation and env configuration run:
+`npm run start`
+
+**And there we go!**
+
+
+
---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const schedule = require('node-schedule');
+const messages = [
+  "Armen el among us, culos",
+  "Armen el among us, puñetas",
+  "Quién vergas es el impostor?",
+  "Tiempo de purgar la nave, culeros",
+  "Hora de la milonga",
+  "No me ignoren, ogts!",
+];
+
+require('dotenv').config()
+if(!process.env.DISCORD_BOT_TOKEN && !process.env.CRONTAB_SCHEDULE) {
+  console.error("Environment not properly configured!")
+  process.exit(0)
+}
+
+const client = new Discord.Client();
+const botToken = process.env.DISCORD_BOT_TOKEN;
+const crontab = process.env.CRONTAB_SCHEDULE;
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  let chan = client.channels.cache.find(c => c.type == 'text' && c.name == 'general')
+  if(chan){
+    schedule.scheduleJob(crontab, function() {
+      chan.send(messages[Math.random() * messages.length | 0])
+      console.log('message sended!')
+    })
+  }
+})
+
+client
+  .login(botToken)
+  .catch(e => console.error(e))
